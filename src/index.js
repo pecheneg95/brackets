5a8be11d28498d52f8c9c898f647147c1d2e4930
Artyom <pechera20@gmail.com>
2022-07-13 23:54:37 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  function deleteBrackets(str, bracketsConfig) {
+    for (let i = 0; i < bracketsConfig.length; i++) {
+      if (bracketsConfig[i] && str.includes(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`)) {
+        str = str.replace(`${bracketsConfig[i][0]}${bracketsConfig[i][1]}`, "");
+        return deleteBrackets(str, bracketsConfig);
+      }
+    }
+    return str;
+  }
+  let res = deleteBrackets(str, bracketsConfig);
+  return res === "";
+};
